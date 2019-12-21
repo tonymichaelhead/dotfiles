718fa816b7723d94eea2e5ddd0424243f3888799
tonymichaelhead <tonymichaelhead@gmail.com>
2019-12-21 00:09:04 -0800

    Update polybar modules and add autodisplay

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,8 +7,16 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log # /tmp/polybar2.log
-polybar example >>/tmp/polybar1.log 2>&1 &
+# echo "---" | tee -a /tmp/polybar1.log # /tmp/polybar2.log
+# polybar example >>/tmp/polybar1.log 2>&1 &
 # polybar bar2 >>/tmp/polybar2.log 2>&1 &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Bars launched..."
